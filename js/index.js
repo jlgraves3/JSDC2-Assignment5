eb2156a599a49273fd66fb97434cc8fdd3109cb1
Julia Graves <jgraves@wesleyan.edu>
2016-07-18 20:59:58 -0400

    my first commit

---
+++ js/index.js
@@ -0,0 +1,48 @@
+//Model
+var count = {
+	number: 0,
+	negative: false,
+	over100: false,
+};
+
+
+//View
+function renderCount() {
+	$('h2').html("");
+	var source = $('#count-template').html();
+	var template = Handlebars.compile(source);
+	var renderedTemplate = template(count);
+	$('body').append(renderedTemplate);
+}
+
+//Controller
+function setup() {
+	renderCount();
+	$('#plus').on('click',increment);
+	$('#minus').on('click',decrement);
+	$('#reset').on('click',resetCount);
+}
+
+function updateCount() {
+	count.negative = (count.number < 0) ? true : false;
+	count.over100 = (count.number > 100) ? true : false;
+}
+
+function increment(){
+	count.number++;
+	updateCount();
+	renderCount();
+}
+function decrement(){
+	count.number--;
+	updateCount();
+	renderCount();
+}
+
+function resetCount(){
+	count.number = 0;
+	updateCount();
+	renderCount();
+}
+
+$(document).ready(setup);
